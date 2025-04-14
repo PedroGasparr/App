6196cb859f8c742b0b806c6d1f70cd5434b16961
Pedro Henrique Gaspar <106278541+PedroGasparr@users.noreply.github.com>
2025-04-14 18:24:39 -0300

    2.4.6

---
+++ js.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDZwa4XNqlupNDLs7-CRQbhr5pRT4NyFBA",
+    authDomain: "application-gzl.firebaseapp.com",
+    databaseURL: "https://application-gzl-default-rtdb.firebaseio.com",
+    projectId: "application-gzl",
+    storageBucket: "application-gzl.firebasestorage.app",
+    messagingSenderId: "319900903265",
+    appId: "1:319900903265:web:a8f400aeb7a697fc168720",
+    measurementId: "G-ZRRFQZXT54"
+};
